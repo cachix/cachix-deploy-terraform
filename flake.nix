f4e62e1ccb977a6a7da4f0e4f22f7c1d9d227061
sandydoo <hey@sandydoo.me>
2022-10-18 23:48:54 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description =
+    "A Terraform module to set up machines managed by Cachix Deploy";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell { buildInputs = [ pkgs.terraform ]; };
+      });
+}
